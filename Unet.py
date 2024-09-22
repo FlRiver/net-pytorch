8ec1ae6c082745b288d592aa06aa86726877f394
SuiJiangPiaoLiu <69428250+FlRiver@users.noreply.github.com>
2024-09-22 10:10:54 +0800

    Update Unet.py

--- Unet.py
+++ Unet.py
@@ -1,34 +1,47 @@
 import torch
 class UNet(nn.Module):
-    def __init__(self,img_channel,channels=[64, 128, 256, 512, 1024],time_emb_size=256,qsize=16,vsize=16,fsize=32,cls_emb_size=32):
-        super().__init__()
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
 
-        channels=[img_channel]+channels
-        
-        # time转embedding
-        self.time_emb=nn.Sequential(
-            TimePositionEmbedding(time_emb_size),
-            nn.Linear(time_emb_size,time_emb_size),
-            nn.ReLU(),
-        )
+        self.inc = (DoubleConv(n_channels, 64))
+        self.down1 = (Down(64, 128))
+        self.down2 = (Down(128, 256))
+        self.down3 = (Down(256, 512))
+        factor = 2 if bilinear else 1
+        self.down4 = (Down(512, 1024 // factor))
+        self.up1 = (Up(1024, 512 // factor, bilinear))
+        self.up2 = (Up(512, 256 // factor, bilinear))
+        self.up3 = (Up(256, 128 // factor, bilinear))
+        self.up4 = (Up(128, 64, bilinear))
+        self.outc = (OutConv(64, n_classes))
 
-        # 引导词cls转embedding
-        self.cls_emb=nn.Embedding(10,cls_emb_size)
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
 
-        # 每个encoder conv block增加一倍通道数
-        self.enc_convs=nn.ModuleList()
-        for i in range(len(channels)-1):
-            self.enc_convs.append(ConvBlock(channels[i],channels[i+1],time_emb_size,qsize,vsize,fsize,cls_emb_size))
-        
-        # 每个encoder conv后马上缩小一倍图像尺寸,最后一个conv后不缩小
-        self.maxpools=nn.ModuleList()
-        for i in range(len(channels)-2):
-            self.maxpools.append(nn.MaxPool2d(kernel_size=2,stride=2,padding=0))
-        
-        # 每个decoder conv前放大一倍图像尺寸，缩小一倍通道数
-        self.deconvs=nn.ModuleList()
-        for i in range(len(channels)-2):
-            self.deconvs.append(nn.ConvTranspose2d(channels[-i-1],channels[-i-2],kernel_size=2,stride=2))
+    def use_checkpointing(self):
+        self.inc = torch.utils.checkpoint(self.inc)
+        self.down1 = torch.utils.checkpoint(self.down1)
+        self.down2 = torch.utils.checkpoint(self.down2)
+        self.down3 = torch.utils.checkpoint(self.down3)
+        self.down4 = torch.utils.checkpoint(self.down4)
+        self.up1 = torch.utils.checkpoint(self.up1)
+        self.up2 = torch.utils.checkpoint(self.up2)
+        self.up3 = torch.utils.checkpoint(self.up3)
+        self.up4 = torch.utils.checkpoint(self.up4)
+        self.outc = torch.utils.checkpoint(self.outc)
 
         # 每个decoder conv block减少一倍通道数
         self.dec_convs=nn.ModuleList()
